be1cc29ac320d12d025639235b9d7b96dc5bd1bc
theazra <bajrektarevicazra0@gmail.com>
2025-04-17 11:20:46 +0200

    PDF

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -19,7 +19,7 @@ const LatestCollection = () => {
         <div className='text-center py-8 text-3xl'> 
         <Title text1={'LATEST'} text2={'COLLECTION'} />
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-            Lorem ipsum
+        Our most-loved finds. Limited pieces — shop now!
         </p>
         </div>
 
